bf0b7f9da378cadd776e72b5b6f87cfe1878ea89
EsterSPR <web.esterspreafico@gmail.com>
2023-02-01 17:35:20 +0100

    creato layout statico

--- index.php
+++ index.php
@@ -14,7 +14,74 @@
 </head>
 <body>
     
-    
+    <div class="container">
+        <div class="row text-center mb-4">
+            <h1>Strong Password Generator</h1>
+            <h2>Genera una password sicura</h2>
+        </div>
+        <div class="row bg-white p-5 rounded">
+            <div class="col-12">
+                <form action="">
+                    <div class="row py-3">
+                        <div class="col-6">
+                            <label for="pwdLength" class="form-label">Lunghezza password:</label>
+                        </div>
+                        <div class="col-6">
+                            <input type="text" class="form-control" id="pwdLength" required>
+                        </div>
+                    </div>
+                    <div class="row py-3">
+                        <div class="col-6">
+                            <label for="pwdRepeat" class="form-label">Consenti ripetizioni di uno o più caratteri:</label>
+                        </div>
+                        <div class="col-6">
+                            <div class="form-check" id="pwdRepeat">
+                                <input class="form-check-input" type="radio" name="pwdRepeat" id="pwdRepeatYes" checked>
+                                <label class="form-check-label" for="pwdRepeatYes">
+                                    Si
+                                </label>
+                                </div>
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio" name="pwdRepeat" id="pwdRepeatNo">
+                                <label class="form-check-label" for="pwdRepeatNo">
+                                    No
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row py-3">
+                        <div class="col-6"></div>
+                        <div class="col-6">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="pwdCharacter1">
+                                <label class="form-check-label" for="pwdCharacter1">
+                                    Lettere
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="pwdCharacter2">
+                                <label class="form-check-label" for="pwdCharacter2">
+                                    Numeri
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="pwdCharacter3">
+                                <label class="form-check-label" for="pwdCharacter3">
+                                    Simboli
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row py-3">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary">Invia</button>
+                            <button class="btn btn-secondary">Annulla</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
     <script src="./js/script.js"></script>
 </body>
